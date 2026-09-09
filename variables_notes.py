5817c5f41c5586db2a09f457e90b47be5e75e61f
marcomorales-stack <marco.morales@ucas-edu.net>
2026-09-09 14:52:46 -0600

    Variables Notes

---
+++ variables_notes.py
@@ -0,0 +1,7 @@
+# MM, Variables Notes <= for people to read not the computer
+
+name = input ("tell me your name: ")
+
+print("CSP is the best calss ever!!!!!")
+print("Pink classroom is the best")
+print("hello,name,"welcom to class!)
