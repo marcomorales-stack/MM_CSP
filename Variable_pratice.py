4e1c61d10ffdbc454e43bd6094382cdc93a3a99d
marcomorales-stack <marco.morales@ucas-edu.net>
2026-09-11 14:41:01 -0600

    Variable pratice

---
+++ Variable_pratice.py
@@ -0,0 +1,42 @@
+# MM, Variable pratice
+
+name = "Marco"
+
+number_1_to_10 = 7
+
+number_100_to_1000 = 67
+
+breakfast = "eggs"
+
+favorite_color = "blue"
+
+school_name = "UCAS"
+
+year = 2026
+
+eye_color = "brown"
+
+age = 14
+
+favorite_subject = "Math"
+
+
+print(name)
+
+print(number_1_to_10)
+
+print(number_100_to_1000)
+
+print(breakfast)
+
+print(favorite_color)
+
+print(school_name)
+
+print(year)
+
+print(eye_color)
+
+print(age)
+
+print(favorite_subject)
--- variables_notes.py
+++ variables_notes.py
@@ -4,4 +4,4 @@ name = input ("tell me your name: ")
 
 print("CSP is the best calss ever!!!!!")
 print("Pink classroom is the best")
-print("hello,name,"welcom to class!)
+print("hello,name,"welcom to class!")
