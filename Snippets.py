ecae129d9af77bbaffa6a1919ca6548a3d724c07
marcomorales-stack <marco.morales@ucas-edu.net>
2026-09-14 14:54:02 -0600

    Snippets.py

---
+++ Snippets.py
@@ -0,0 +1,28 @@
+# MM, snippets
+
+name = "Marco"
+print(f"The varible name has a value of {name} and is saved at {id(name)}")
+
+age = 14
+print(f"The varible name has a value of {age} and is saved at {id(age)}")
+
+score = 10
+print(f"The varible name has a value of {score} and is saved at {id(score)}")
+
+score = 25
+print(f"The varible name has a value of {score} and is saved at {id(score)}")
+
+height = 5.6
+print(f"The varible name has a value of {height} and is saved at {id(height)}")
+
+city = "Lindon"
+print(f"The varible name has a value of {city} and is saved at {id(city)}")
+
+zip_code = 84042
+print(f"The varible name has a value of {zip_code} and is saved at {id(zip_code)}")
+
+price1 = 10
+print(f"The varible name has a value of {price1} and is saved at {id(price1)}")
+
+price2 = 10
+print(f"The varible name has a value of {price2} and is saved at {id(price2)}")
