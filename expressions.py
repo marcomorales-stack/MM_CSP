1301826e054076336ae3fbe0fa239687f2aa61e2
marcomorales-stack <marco.morales@ucas-edu.net>
2026-09-16 14:52:45 -0600

    expressions.py

---
+++ expressions.py
@@ -0,0 +1,31 @@
+# MM 7th Intergers, Floats, and Expressions notes
+
+# Interger => a whole number
+students = 23
+cars = 50
+computers = 29
+awareness = -12
+
+# Float=> numbers with decimals
+pi = 3.1415
+temp = -95.6
+cost = 1.99
+rain = 2.17
+
+#Arithmetic Operators (+ - * / ** // %)
+print(f"18/4 is {18/4} or {18//4} the remander of {18%4}")
+print(f"18/5 is {18/5} or {18//5} the remander of {18%5}")
+
+# order of operations
+grades = [85,66,94,72,88,79,100]
+students == len(grades)
+average = sum(grades)/students
+
+print(f"The average is {average}")
+
+# Convert the data type 
+price = input("How much did the item cost: ")
+tax = 0.0485
+sales_tax = price * tax
+total = price + sales_tax 
+print(f"Your total is {total}")
