1b559b2721c76a6351e8d31262962d723454182b
marcomorales-stack <marco.morales@ucas-edu.net>
2026-09-17 14:51:04 -0600

    Your_Budget.

---
+++ Your_Budget.py
@@ -0,0 +1,40 @@
+# MM, Your_Budget
+
+while True:
+    try:
+        income = float(input("What is your monthly income"))
+        break
+    except:
+        print("Thats not a number")
+
+   
+while True:
+    try:
+        income = float(input("What is your monthly rent/mortgage"))
+        break
+    except:
+        print("Thats not a number")
+
+   
+while True:
+    try:
+        income = float(input("What is your monthly utilities"))
+        break
+    except:
+        print("Thats not a number")
+
+   
+while True:
+    try:
+        income = float(input("What is your monthly groceries"))
+        break
+    except:
+        print("Thats not a number")
+
+    
+while True:
+    try:
+        income = float(input("What is your monthly transportation"))
+        break
+    except:
+        print("Thats not a number")
