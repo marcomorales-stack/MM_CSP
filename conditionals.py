5c2d67da0942274b872900af0e6675aa2625c063
marcomorales-stack <marco.morales@ucas-edu.net>
2026-09-22 14:51:41 -0600

    conditionals.

---
+++ conditionals.py
@@ -0,0 +1,18 @@
+# MM, 7th, Conditional Notes
+
+# conditional
+
+time = 1416
+day = "Tuesday"
+
+if time < 1200 and time > 500:
+    print("Good Morning!")
+elif time < 1700:
+        if day != "Saturday" and
+    print("Good Afternoon!")
+elif time < 2000:
+    print("Good Evening")
+else:
+    print("Good Night!")
+
+print("Code is done")
